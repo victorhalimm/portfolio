ea097ecc55b0c3ed6acff2851192af35829d12c1
victorhalimm <victorhalim18@gmail.com>
2024-09-28 09:48:51 +0700

    feat: font change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
 				'background' : '#13151a'
-			}
+			},
+			fontFamily: {
+				sans: ['SourceSans3', ...defaultTheme.fontFamily.sans],
+			},
 		},
 	},
 	plugins: [],
