2acce46bd582e4382e965c2e0380c6ec104afbc5
Victor Halim <victorhalim18@gmail.com>
2026-03-17 18:54:22 +0700

    feat: change color and font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,25 +2,26 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-    	extend: {
-    		colors: {
-    			background: '#13151a',
-    			accent: '#cc9ae0',
-    			content: '#9ca3af',
-    			subtle: '#6b7280'
-    		},
-    		fontFamily: {
-    			sans: ['SourceSans3', ...defaultTheme.fontFamily.sans]
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		}
-    	}
-    },
-	plugins: [require("tailwindcss-animate")],
+  darkMode: ['class'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#13151a',
+        accent: '#8ec5ff',
+        content: '#9ca3af',
+        subtle: '#6b7280'
+      },
+      fontFamily: {
+        sans: ['iA Writer Quattro', ...defaultTheme.fontFamily.sans],
+        serif: ['IBM Plex Serif', ...defaultTheme.fontFamily.serif]
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
+  },
+  plugins: [require('tailwindcss-animate')]
 }
